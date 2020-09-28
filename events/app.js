00be831589a80631f69fc90679bf71cc3c12182f
Antonio Erdeljac <hedhogs@gmail.com>
2020-09-28 17:20:09 +0200

    event binding

--- events/app.js
+++ events/app.js
@@ -1,7 +1,14 @@
 new Vue({
   el: '#vue-app',
   data: {
+    age: 25,
   },
   methods: {
+    addYear: function() {
+      this.age = this.age + 1;
+    },
+    subtractYear: function() {
+      this.age = this.age - 1;
+    }
   }
 });
