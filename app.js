711313b5ca7090264d66040c23240e2d9536a9a9
Antonio Erdeljac <hedhogs@gmail.com>
2020-09-28 17:08:40 +0200

    data binding

--- app.js
+++ app.js
@@ -3,5 +3,11 @@ new Vue({
   data: {
     name: 'Vue app',
     job: 'test',
+    website: 'https://www.google.com/',
+  },
+  methods: {
+    greet: function(name) {
+      return `Hello ${name}. Your job is ${this.job}`
+    },
   }
 });
