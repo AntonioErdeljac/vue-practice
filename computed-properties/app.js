d8c1b072dfaccc1c1d7b1b420359f9fa5ab4d19a
Antonio Erdeljac <hedhogs@gmail.com>
2020-09-28 18:17:03 +0200

    Add computed values

--- computed-properties/app.js
+++ computed-properties/app.js
@@ -1,7 +1,13 @@
 new Vue({
   el: '#vue-app',
   data: {
+    a: 0,
+    b: 0,
+    age: 20,
   },
   methods: {
+    addTo: function (value) {
+      return this[value] + this.age;
+    }
   }
 });
