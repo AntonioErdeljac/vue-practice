833f9229ec3418560bcc928cb64e5b201ca974fa
Antonio Erdeljac <hedhogs@gmail.com>
2020-09-28 17:27:45 +0200

    link fix

--- events/app.js
+++ events/app.js
@@ -4,11 +4,11 @@ new Vue({
     age: 25,
   },
   methods: {
-    addYear: function() {
-      this.age = this.age + 1;
+    add: function(amount) {
+      this.age += amount;
     },
-    subtractYear: function() {
-      this.age = this.age - 1;
+    subtract: function(amount) {
+      this.age -= amount;
     }
   }
 });
