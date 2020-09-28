65eddc87e4f17313224c4a87596ffcbeeb7a180d
Antonio Erdeljac <hedhogs@gmail.com>
2020-09-28 17:14:36 +0200

    Html binding

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ new Vue({
     name: 'Vue app',
     job: 'test',
     website: 'https://www.google.com/',
+    websiteTag: '<a href="https://www.google.hr/">Website #2</a>',
   },
   methods: {
     greet: function(name) {
