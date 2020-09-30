97b3c5d03d39f89946be9253a9b22d4d0019ffc3
Antonio Erdeljac <hedhogs@gmail.com>
2020-09-30 21:05:25 +0200

    Add v-for binding

---
+++ looping/app.js
@@ -0,0 +1,18 @@
+new Vue({
+  el: '#vue-app',
+  data: {
+    characters: ['Antonio', 'Marko', 'Ivan'],
+    users: [
+      {
+        name: 'Antonio',
+        age: 19,
+      },
+      {
+        name: 'Marko',
+        age: 20,
+      }
+    ],
+  },
+  methods: {
+  }
+});
