0f064f3535c9fc3b18d5ace1526fef739582ae0f
Antonio Erdeljac <hedhogs@gmail.com>
2020-10-04 16:48:19 +0200

    Add multiple instances example

--- multiple-instances/app.js
+++ multiple-instances/app.js
@@ -1,4 +1,30 @@
-new Vue({
-  el: '#vue-app',
-  data: {},
+const one = new Vue({
+  el: '#vue-app-one',
+  data: {
+    title: 'App #1'
+  },
+  computed: {
+    greet: function() {
+      return 'Hello App #1'
+    }
+  }
 });
+
+const two = new Vue({
+  el: '#vue-app-two',
+  data: {
+    title: 'App #2'
+  },
+  methods: {
+    changeTitle: function() {
+      one.title = 'Test'
+    }
+  },
+  computed: {
+    greet: function() {
+      return 'Hello App #2'
+    }
+  }
+});
+
+two.title = 'Changed from outside'
